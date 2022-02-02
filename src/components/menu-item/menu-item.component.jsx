a72655962d5d39ba84bbe6192b491d1c42c82b24
BO0Mbox <parikshithshetty2623@gmail.com>
2022-02-02 10:22:48 +0530

    update our menu-items to navigate withRouter function

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 
 import './menu-item.styles.scss';
+import { withRouter } from "react-router-dom";
 
-const MenuItem = ({title, imageUrl,size}) => (                                                // receiving parameters from directory
+const MenuItem = ({title, imageUrl,size,history,linkUrl,match}) => (                                                // receiving parameters from directory
     <div
       
       className={`${size} menu-item`}
+      onClick={() => history.push(`${match.url}${linkUrl}`)}
     >
           <div className="background-image" style={{
             backgroundImage:`url(${imageUrl})`                              // string interpolation of url - $
@@ -17,4 +19,4 @@ const MenuItem = ({title, imageUrl,size}) => (                                  
         </div>
     );
 
-export default MenuItem;
+export default withRouter(MenuItem);
